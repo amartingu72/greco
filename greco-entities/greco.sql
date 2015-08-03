afe0883b584b07b55c729f701c073b33d08ef291
amartingu72 <amartingu72@gmail.com>
2015-08-03 11:08:07 +0200

    Fix #39. Corregimos error de commit anterior.

---
+++ greco-entities/greco.sql
@@ -0,0 +1,117 @@
+
+
+create table users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	nickname varchar(32) not null,
+	email varchar(64) not null,
+	profile varchar (32) not null DEFAULT 'USER',
+	mydata varchar(256),
+	password varchar(16)
+);
+
+create table countries (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(48) not null
+);
+
+create table communities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(32) not null,
+	notes varchar(256),	
+	zipcode varchar(5) not null,
+	countrie_id int not null,
+	available TINYINT not null,
+	FOREIGN KEY (countrie_id) REFERENCES countries(id)
+);
+
+create table profiles (
+	id INT AUTO_INCREMENT PRIMARY KEY,	
+	profile varchar(16) not null,
+	description varchar(64)
+);
+insert into profiles (profile,description) values ('Administrador', 'Administrador de la comunidad'), ('Copropietario', 'Copropietario de recursos');
+
+create table resourcetypes (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(32) not null,
+	description varchar(128)
+);
+
+create table timeunit (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name varchar(32) not null
+);
+-- 
+	
+-- Para Community poner un campo donde se indiquen notas (notes), por ejemplo, en comunidades restringidas (donde hace falta dni), indicar cómo 
+
+create table resources (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	name varchar(32),
+	description varchar(128),
+	minTime	int not null comment 'Tiempo mínimo reservable en minutos. Conforma las unidades de reserva',
+	maxTime int not null comment 'Tiempo máximo reservable en minutos',
+	availableFromTime varchar(5) comment 'Disponible desde las 00:00',
+	availableToTime	varchar(5) comment 'Disponible hasta  las 23:59',
+	beforehand int,
+	resourceType_id int not null,
+	community_id int not null,
+	beforehandtu_id int not null DEFAULT 1 comment 'Unidad de tiempo para beforehand.',
+	timeunit_id int not null DEFAULT 1 comment 'Unidad de tiempo para maxtime y mintime.',
+	FOREIGN KEY (resourceType_id) REFERENCES resourcetypes(id),
+	FOREIGN KEY (beforehandtu_id) REFERENCES timeunit(id),
+	FOREIGN KEY (timeunit_id) REFERENCES timeunit(id),
+	FOREIGN KEY (community_id) REFERENCES communities (id)
+);
+
+
+create table users_communities (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	community_id int not null,
+	user_id  int not null,
+	profile_id  int not null,
+	registerDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                  ON UPDATE CURRENT_TIMESTAMP,
+	status int not null default 0,
+	FOREIGN KEY (community_id) REFERENCES communities (id),
+	FOREIGN KEY (user_id) REFERENCES users (id),
+	FOREIGN KEY (profile_id) REFERENCES profiles (id)
+);
+
+
+create table exceptions (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	resources_id int not null,
+	fromDate TIMESTAMP not null,
+	toDate TIMESTAMP,
+	FOREIGN KEY (resources_id) REFERENCES resources (id)
+);
+
+
+
+create table reservations (
+	id int AUTO_INCREMENT PRIMARY KEY,
+	user_id int not null,
+	resource_id int not null,
+	fromDate timestamp not null default 0,
+	toDate timestamp not null null default 0,
+	status int not null default 0,
+	FOREIGN KEY (user_id) REFERENCES users(id),
+	FOREIGN KEY (resource_id) REFERENCES resources(id)	
+);
+
+
+
+insert into timeunit set name="Horas";
+insert into timeunit set name="Minutos";
+insert into timeunit set name="Días";
+
+insert into resourcetypes values (DEFAULT, 'tenis', 'Pista de tenis');
+insert into resourcetypes values (DEFAULT, 'pingpong', 'Tenis de mesa');
+insert into resourcetypes values (DEFAULT, 'padel', 'Pista de pádel');
+insert into resourcetypes values (DEFAULT, 'sala', 'Sala de reunión');
+insert into resourcetypes values (DEFAULT, 'parking', 'Plaza de aparcamiento');
+insert into resourcetypes values (DEFAULT, 'citas', 'Concertación cita');
+insert into resourcetypes values (DEFAULT, 'espacio', 'Espacio multiusos');
+
+
