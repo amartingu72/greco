38824b5e73b507d2f9ed72e37b8b939067114378
amartingu72 <amartingu72@gmail.com>
2015-10-06 17:41:21 +0200

    Correcciona de los JNDI para que funcione en JBOSS. Subieron mal en la versión anterior.

--- greco-console/src/main/java/com/greco/services/impl/MailProviderImpl.java
+++ greco-console/src/main/java/com/greco/services/impl/MailProviderImpl.java
@@ -30,7 +30,7 @@ import com.greco.services.helpers.UserItem;
 public class MailProviderImpl implements MailProvider {
 	
 	private final String JNDI_NAME="java:jboss/mail/Default";  //Para JBOSS
-	//private final String JNDI_NAME="mail/gmailAccount";  //Para Glassfish
+	//private final String JNDI_NAME="mail/gmailAccount";  //Para Glassfish.
 	
 	@Resource(name="UsersRepository")
 	private UserDAO usersRepository;
